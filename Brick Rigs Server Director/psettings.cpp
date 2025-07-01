fd8837b71dc416bede1b895694cbe71ed149d6e9
Aaron Wilk <aaron.wilk@icloud.com>
2025-07-01 12:30:33 -0500

    Change overall canvas to a scroll box to match standard implementation with other settings pages.

--- Brick Rigs Server Director/psettings.cpp
+++ Brick Rigs Server Director/psettings.cpp
@@ -20,36 +20,37 @@ bool psettings::CreateCustomSettingsPage()
     MockPage = Spawn(SDK::UMenuPageWidget, GetMenu());
 
 
-    CustomSettingsPage = Spawn(SDK::UCanvasPanel, GetWindowManager());
+    CustomSettingsPage = Spawn(SDK::UBrickScrollBox, GetWindowManager());
+    CustomSettingsPage->SetSlotSpacingStyle(SDK::EBrickUISpacingStyle::Medium);
+    CustomSettingsPage->SetSlotSpacing(10.f);
     CustomSettingsPage->SetVisibility(SDK::ESlateVisibility::Hidden);
     SDK::UCanvasPanelSlot* slot = GetCanvasPanel()->AddChildToCanvas(CustomSettingsPage);
-    slot->SetAutoSize(true);
     SDK::FAnchors anchor = SDK::FAnchors();
-    anchor.Minimum.X = 0.3;
-    anchor.Minimum.Y = 0.2;
-    anchor.Maximum.X = 0.3;
-    anchor.Maximum.Y = 0.2;
+    anchor.Minimum.X = 0.214f;
+    anchor.Minimum.Y = 0.159f;
+    anchor.Maximum.X = 0.993f;
+    anchor.Maximum.Y = 0.988f;
     slot->SetAnchors(anchor);
-    slot->SetAlignment(SDK::FVector2D(1.f, 0.f));
-    slot->SetSize(SDK::FVector2D(200.0f, 200.0f));
+    slot->SetAlignment(SDK::FVector2D(0.f, 0.f));
+    slot->SetSize(SDK::FVector2D(0.0f, 0.0f));
+    slot->SetZOrder(1000);
 
 
     SDK::UBrickTextBlock* TextBlock = Spawn(SDK::UBrickTextBlock, CustomSettingsPage);
     if (!TextBlock) return false;
-    TextBlock->SetText(TEXT(L"Testing!"));
+    TextBlock->SetText(TEXT(L"Brick Rigs Server Director Options"));
+    TextBlock->SetTextStyle(SDK::EBrickUITextStyle::Bold);
     SDK::UBrickBorder* TextBorder = Spawn(SDK::UBrickBorder, CustomSettingsPage);
     if (!TextBorder) return false;
     TextBorder->SetContent(TextBlock);
-    SDK::UCanvasPanelSlot* slot2 = CustomSettingsPage->AddChildToCanvas(TextBorder);
-    slot2->SetAutoSize(true);
-    SDK::FAnchors anchos = SDK::FAnchors();
-    anchos.Maximum = SDK::FVector2D(1.0f, 0.0f);
-    anchos.Minimum = SDK::FVector2D(1.0f, 0.0f);
-    slot2->SetAnchors(anchos);  // Top-Right Align
-    slot2->SetAlignment(SDK::FVector2D(1.f, 0.f));
-    slot2->SetPosition(SDK::FVector2D(0.0f,0.0f));  // x: 20px left, y: 20px down
-
-
+    SDK::UPanelSlot* slot2 = CustomSettingsPage->AddChild(TextBorder);
+    //TextBorder->ForceLayoutPrepass(); //Forces the correct size
+    //slot2->SetAutoSize(true);
+    //SDK::FAnchors anchos = SDK::FAnchors();
+    //anchos.Maximum = SDK::FVector2D(0.5f, 0.0f);
+    //anchos.Minimum = SDK::FVector2D(0.5f, 0.0f);
+    //slot2->SetAnchors(anchos);
+    //slot2->SetAlignment(SDK::FVector2D(0.5f, 0.0f));
     SynchronizeProperties(TextBorder);
 
     ElementsList.push_back(TextBorder);
@@ -64,12 +65,8 @@ void psettings::SetVisibility(SDK::ESlateVisibility vis)
 
 void psettings::Uninitalize()
 {
+    ElementsList.clear();
+    CustomSettingsPage->ClearChildren();
+    CustomSettingsPage->RemoveFromParent();
     CustomSettingsPage == nullptr;
-    for (int i = 0; i < ElementsList.size(); i++)
-    {
-        if (ElementsList[i]) {
-            ElementsList[i]->RemoveFromParent();
-        }
-        ElementsList[i] = nullptr;
-    }
 }
